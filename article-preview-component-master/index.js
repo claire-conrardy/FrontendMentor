5083be66813e9477954680bc270b58ff1681f5a8
Claire Conrardy (She/Her) <claire.conrardy@gmail.com>
2022-06-06 12:26:50 -0400

    Added the Testimonials Grid challenge to the directory

--- article-preview-component-master/index.js
+++ article-preview-component-master/index.js
@@ -1,27 +1,29 @@
 // View and hide Sharing buttons
-let share = document.querySelector('.share-btn');
-let sharingBtns = document.querySelector(".sharing-btns");
-let userInfo = document.querySelector('.user-info');
-let sharingBtnsMobile = document.querySelector(".sharing-btns-mobile");
-sharingBtns.style.display = "none";
+// let share = document.querySelector('.share-btn');
+// let sharingBtns = document.querySelector(".sharing-btns");
+// let userInfo = document.querySelector('.user-info');
+// let sharingBtnsMobile = document.querySelector(".sharing-btns-mobile");
+// sharingBtns.style.display = "none";
+// sharingBtnsMobile.style.display = "none";
 
-let open = () => {
-  if (window.matchMedia("(min-width: 600px)").matches) {
-    if (sharingBtns.style.display === "none") {
-      sharingBtns.style.display = "flex";
-    } else {
-      sharingBtns.style.display = "none";
-    }
-  } else {
-    if (sharingBtnsMobile.style.display === "none") {
-      sharingBtnsMobile.style.display = "flex";
-      userInfo.style.display = "none";
-    } else {
-      sharingBtnsMobile.style.display = "none";
-      userInfo.style.display = "flex";
-    }
+// let open = () => {
+//   if (sharingBtns.style.display === "none") {
+//     sharingBtns.style.display = "flex";
+//   } else {
+//     sharingBtns.style.display = "none";
+//   }
+// }
+//   share.onclick = open;
 
-  }
-}
+  // if (window.matchMedia("(min-width: 600px)").matches) {
+  // }
+  // } else {
+  //   if (sharingBtnsMobile.style.display === "none") {
+  //     sharingBtnsMobile.style.display = "flex";
+  //     userInfo.style.display = "none";
+  //   } else {
+  //     sharingBtnsMobile.style.display = "none";
+  //     userInfo.style.display = "flex";
+  //   }
 
-share.onclick = open;
+  // }
